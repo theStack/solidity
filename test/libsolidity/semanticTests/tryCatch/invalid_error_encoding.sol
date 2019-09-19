4337f273927b328f8079b39aaf7ca24c56cbad46
chriseth <chris@ethereum.org>
2019-09-19 13:37:01 +0200

    [SQUASH] tests

--- test/libsolidity/semanticTests/tryCatch/invalid_error_encoding.sol
+++ test/libsolidity/semanticTests/tryCatch/invalid_error_encoding.sol
@@ -30,6 +30,14 @@ contract C {
             return 1;
         }
     }
+    function f1c() public returns (uint x) {
+        // Invalid signature
+        try this.g(abi.encodeWithSelector(0x12345678, uint(0), uint(0), uint(0))) returns (uint a, uint b) {
+            return 0;
+        } catch {
+            return 2;
+        }
+    }
     function f2() public returns (uint x) {
         // Valid signature but illegal offset
         try this.g(abi.encodeWithSignature("Error(string)", uint(0x100), uint(0), uint(0))) returns (uint a, uint b) {
@@ -55,6 +63,14 @@ contract C {
         try this.g(abi.encodeWithSignature("Error(string)", uint(0x100), uint(0), uint(0))) returns (uint a, uint b) {
             return 0;
         } catch Error(string memory) {
+            return 1;
+        }
+    }
+    function f2c() public returns (uint x) {
+        // Valid signature but illegal offset
+        try this.g(abi.encodeWithSignature("Error(string)", uint(0x100), uint(0), uint(0))) returns (uint a, uint b) {
+            return 0;
+        } catch {
             return 1;
         }
     }
@@ -86,6 +102,14 @@ contract C {
             return 1;
         }
     }
+    function f3c() public returns (uint x) {
+        // Valid up to length
+        try this.g(abi.encodeWithSignature("Error(string)", uint(0x20), uint(0x30), uint(0))) returns (uint a, uint b) {
+            return 0;
+        } catch {
+            return 1;
+        }
+    }
     function f4() public returns (uint x) {
         // Fully valid
         try this.g(abi.encodeWithSignature("Error(string)", uint(0x20), uint(0x7), bytes7("abcdefg"))) returns (uint a, uint b) {
@@ -114,6 +138,14 @@ contract C {
             return 1;
         }
     }
+    function f4c() public returns (uint x) {
+        // Fully valid
+        try this.g(abi.encodeWithSignature("Error(string)", uint(0x20), uint(0x7), bytes7("abcdefg"))) returns (uint a, uint b) {
+            return 0;
+        } catch {
+            return 1;
+        }
+    }
 }
 // ====
 // EVMVersion: >=byzantium
@@ -121,12 +153,16 @@ contract C {
 // f1() -> 2
 // f1a() -> 2
 // f1b() -> FAILURE, hex"12345678", 0x0, 0, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+// f1c() -> 2
 // f2() -> 2
 // f2a() -> 2
 // f2b() -> FAILURE, hex"08c379a0", 0x100, 0, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+// f2c() -> 1
 // f3() -> 2
 // f3a() -> 2
 // f3b() -> FAILURE, hex"08c379a0", 0x20, 48, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+// f3c() -> 1
 // f4() -> 1
 // f4a() -> 1
 // f4b() -> 1
+// f4c() -> 1
